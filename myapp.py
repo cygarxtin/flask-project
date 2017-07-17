73fc16607e1f9f16f3f27998cfa306ca117dae09
Assigeh Delanyo <delanyoassigeh@gmail.com>
2017-07-17 14:20:09 +0000

    initial commit

--- myapp.py
+++ myapp.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Hello"
-#route for web server
+	return render_template("index.html", to="cygarxtin")
+
+@app.route('/css/<path:path>')
+def send_css(path):
+	return send_from_directory('css', path)
+
+@app.route('/img/<path:path>')
+def send_img(path):
+        return send_from_directory('img', path)
+
 @app.route('/whereami')
 def whereami():
 	return "Kdua"
+
+@app.route('/hello/<name>')
+def foo(name):
+        return render_template('index.html', to=path)
+
+@app.route('/python')
+def mypython():
+	return render_template("webpage2.html")
+
+@app.route('/linux')
+def mylinux():
+	return render_template("webpage3.html")
 
 if __name__== '__main__':
 	app.run(host="0.0.0.0")
